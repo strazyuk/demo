a1b6abab5cf9c2a740f31d218f1b63cd15225853
strazyuk <129723342+strazyuk@users.noreply.github.com>
2024-08-09 20:41:51 +0600

    dom aint working idk why

--- leetcode.py
+++ leetcode.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def removeDuplicates(self, nums):
         j = 0
-        for i in range(0, len(nums)):
+        for i in range(1, len(nums)):
             if nums[j] != nums[i]:
                 j += 1
+
                 nums[j] = nums[i]
+
                 
 
         return j + 1  
--- second.js
+++ second.js
@@ -1,8 +1,30 @@
 function mathmaker(firstnum,secondnum){
     var result;
-    result+="The number would be"+firstnum+secondnum;
+    result+="The number would be"+firstnum+' '+secondnum;
     return result
 
 }
 var x =mathmaker(1,56);
 console.log(x)
+
+const object = { 
+    firstname:"asir",
+    lastname:"abrar",
+    fullname: function(){
+        return this.firstname+" "+ this.lastname
+
+    }
+
+};
+// document.getElementById("demo").innerHTML=object;
+
+console.log(object.fullname().toUpperCase());
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  // Display Properties
+  document.getElementById("demo").innerHTML =
+  person.name + "," + person.age + "," + person.city;
